7b25240a2c21bf8370fc3a258916284e4fc8e8fa
kelechi2020 <egbosikelechi@gmail.com>
2017-08-01 10:48:41 -0700

    solved issue os attic files dir

--- work_manager/settings.py
+++ work_manager/settings.py
@@ -9,18 +9,20 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 import os
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, '../Tasksmanager/static'),
+)
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 TEMPLATE_DIRS = (
   os.path.join(PROJECT_ROOT, '../TasksManager/templates'),
   # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
   # Always use forward slashes, even on Windows.
   # Don't forget to use absolute paths, not relative paths.
-)
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
 )
 
 # Quick-start development settings - unsuitable for production
@@ -106,6 +108,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATIC_URL = '/static/'
+
+
+
