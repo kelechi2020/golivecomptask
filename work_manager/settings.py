ae2baa65736293b2501f66df803eb4de3185b259
kelechi2020 <egbosikelechi@gmail.com>
2017-08-01 16:29:38 -0700

    database migrate

--- work_manager/settings.py
+++ work_manager/settings.py
@@ -69,7 +69,7 @@ LOGIN_URL = 'public_connection'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-"""
+
 
 DATABASES = {
     'default': {
@@ -81,16 +81,16 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+
 """
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+"""
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
